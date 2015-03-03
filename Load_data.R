5912b325fd27521a283ba32c84d3c0012a86d7c0
Philippe-C <philippe.chiaroni@hec.edu>
2015-03-03 10:37:53 +0100

    Shows how to read data

---
+++ Load_data.R
@@ -0,0 +1,19 @@
+# STEP 1: Loading the large dataset.
+filename <- "./data/household_power_consumption.txt"
+data <- read.table(filename,
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+dim(data) 
+# CHECK SIZE FOR MEMORY: ROWS AND COLS /O2075259/9
+attach(data)
+# STEP 2: Dataset limited to 2 specific dates
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+newData <- data[subset, ]
+attach(newData)
+x <- paste(Date, Time)
+newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newData) <- 1:nrow(newData)
+dim(newData) # 2880   10
+attach(newData)
