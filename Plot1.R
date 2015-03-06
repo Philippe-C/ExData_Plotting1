41dd3c4811b44dd86a9184e0298a9fe849d5b302
Philippe-C <philippe.chiaroni@hec.edu>
2015-03-06 15:41:16 +0100

    Assignment week 1

--- Plot1.R
+++ Plot1.R
@@ -1,8 +1,8 @@
-# Step 1: Check the presence of the file for loading data
+# Step 1: Check the presence of the file for loading data - Please refer to the load_data.R file in the repo!
 if (!"load_data.R" %in% list.files()) {
         setwd("~/ExData_Plotting1")
 } 
-## Step2: Access the data
+## Step2: Access the data 
 source("load_data.R")
 ### Step 3: Creation of the first plot: an histogram
 png(filename = "plot1.png",width = 480, height = 480)
